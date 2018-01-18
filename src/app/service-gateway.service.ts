d2c0461e55121d1fee9819f183fa34149255cd49
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-01-18 21:18:18 +0200

    update

--- src/app/service-gateway.service.ts
+++ src/app/service-gateway.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
@@ -16,34 +16,52 @@ export class ServiceGatewayService {
 
         const token = localStorage.getItem('token');
 
-        const headers = new HttpHeaders()
-        .set('authorization', `Bearer ${token}`);
+        let headers = new HttpHeaders()
+    
+        if (token) {
+            headers = headers.set('authorization', `Bearer ${token}`);
+        }
 
         const queryString = params ? Object.keys(params).map((key) => `${key}=${params[key]}`).join('&') : '';
         return this.http.get<T>(`${this.baseUri}${uri}?${queryString}`, {
             headers,
+            observe: 'response',
+        }).map((res: HttpResponse<T>) => {
+            return res.body;
         });
     }
 
     public post<T>(uri: string, json: any): Observable<T> {
         const token = localStorage.getItem('token');
-
-        const headers = new HttpHeaders()
-        .set('authorization', `Bearer ${token}`);
+        
+        let headers = new HttpHeaders()
+    
+        if (token) {
+            headers = headers.set('authorization', `Bearer ${token}`);
+        }
 
         return this.http.post<T>(`${this.baseUri}${uri}`, json, {
             headers,
+            observe: 'response',
+        }).map((res: HttpResponse<T>) => {
+            return res.body;
         });
     }
 
     public put<T>(uri: string, json: any): Observable<T> {
         const token = localStorage.getItem('token');
 
-        const headers = new HttpHeaders()
-        .set('authorization', `Bearer ${token}`);
+        let headers = new HttpHeaders()
+    
+        if (token) {
+            headers = headers.set('authorization', `Bearer ${token}`);
+        }
 
         return this.http.put<T>(`${this.baseUri}${uri}`, json, {
             headers,
+            observe: 'response',
+        }).map((res: HttpResponse<T>) => {
+            return res.body;
         });
     }
 }
